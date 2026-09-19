050ea5c9d24da160a3d3550e496832547e5f848a
Mr. Albert <4lbert.ns@gmail.com>
2026-09-18 21:39:26 -0300

    atualização: adicionadas 2 novas URLs ao servidor

--- testeGH/server.js
+++ testeGH/server.js
@@ -6,7 +6,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/home', (req, res) => {
-    res.send('Tela home. <br/><br/><br/> Para retornar à tela /, <a href="http://localhost:3000/">clique aqui!</a>');
+    res.send('Tela home. <br/><br/><br/> Para acessar a tela /, <a href="http://localhost:3000/home/abc">clique aqui!</a> <br> Para retornar à tela /, <a href="http://localhost:3000/">clique aqui!</a>');
+});
+
+app.get('/home/abc', (req, res) => {
+    res.send('Tela abc. <br/><br/><br/> Para acessar a tela /abc, <a href="http://localhost:3000/home/abc/123">clique aqui!</a> <br> Para retornar à tela /home, <a href="http://localhost:3000/home">clique aqui!</a>');
+})
+
+app.get('/home/abc/123', (req, res) => {
+    res.send('Tela 123. <br/><br/><br/> Para retornar à tela /abc, <a href="http://localhost:3000/home/abc">clique aqui!</a>');
 });
 
 app.listen(3000, () => {
