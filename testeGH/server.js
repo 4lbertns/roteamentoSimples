a43191c602291e9eeb61fa09ae6d10c0d5499d2b
Mr. Albert <4lbert.ns@gmail.com>
2026-09-19 01:14:50 -0300

    fixed: corrigindo ponto-e-vírgulas

--- testeGH/server.js
+++ testeGH/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Tela /. <br/><br/><br/> Para acessar a tela /home, <a href="http://localhost:3000/home">clique aqui!</a>');
-})
+});
 
 app.get('/home', (req, res) => {
     res.send('Tela home. <br/><br/><br/> Para acessar a tela /, <a href="http://localhost:3000/home/abc">clique aqui!</a> <br> Para retornar à tela /, <a href="http://localhost:3000/">clique aqui!</a>');
@@ -11,7 +11,7 @@ app.get('/home', (req, res) => {
 
 app.get('/home/abc', (req, res) => {
     res.send('Tela abc. <br/><br/><br/> Para acessar a tela /abc, <a href="http://localhost:3000/home/abc/123">clique aqui!</a> <br> Para retornar à tela /home, <a href="http://localhost:3000/home">clique aqui!</a>');
-})
+});
 
 app.get('/home/abc/123', (req, res) => {
     res.send('Tela 123. <br/><br/><br/> Para retornar à tela /abc, <a href="http://localhost:3000/home/abc">clique aqui!</a>');
