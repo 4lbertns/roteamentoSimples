da0e4c31e69adf582bf339cdf997d58e2b4cfde5
Mr. Albert <4lbert.ns@gmail.com>
2026-09-21 15:35:28 -0300

    feat: roteamentos modificados à pastas, basicamente, se tornou uma lista de atividades

--- testeGH/server.js
+++ testeGH/server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Tela /. <br/><br/><br/> Para acessar a tela /home, <a href="http://localhost:3000/home">clique aqui!</a>');
-});
+app.get('/home', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/index.html'))});
 
-app.get('/home', (req, res) => {
-    res.send('Tela home. <br/><br/><br/> Para acessar a tela /, <a href="http://localhost:3000/home/abc">clique aqui!</a> <br> Para retornar à tela /, <a href="http://localhost:3000/">clique aqui!</a>');
-});
-
-app.get('/home/abc', (req, res) => {
-    res.send('Tela abc. <br/><br/><br/> Para acessar a tela /abc, <a href="http://localhost:3000/home/abc/123">clique aqui!</a> <br> Para retornar à tela /home, <a href="http://localhost:3000/home">clique aqui!</a>');
-});
-
-app.get('/home/abc/123', (req, res) => {
-    res.send('Tela 123. <br/><br/><br/> Para retornar à tela /abc, <a href="http://localhost:3000/home/abc">clique aqui!</a>');
-});
+app.get('/home/roteamento', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/roteamento.html'))});
+app.get('/home/roteamento/abc', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/abc.html'))});
+app.get('/home/roteamento/abc/123', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/123.html'))});
 
 app.listen(3000, () => {
-    console.log('Servidor ativo na porta 3000. Acesse http://localhost:3000/');
+    console.log('Servidor ativo na porta 3000. Acesse http://localhost:3000/home');
 });
