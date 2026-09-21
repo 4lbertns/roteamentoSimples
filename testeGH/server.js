d85b14fc6864a6aa628c7ce2f600d4850cc7e52a
Mr. Albert <4lbert.ns@gmail.com>
2026-09-21 18:07:07 -0300

    feat: modificações de roteamento - nova atividade

--- testeGH/server.js
+++ testeGH/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/home', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/index.html'))});
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/view/roteamentoSimples/index.html')));
 
-app.get('/home/roteamento', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/roteamento.html'))});
-app.get('/home/roteamento/abc', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/abc.html'))});
-app.get('/home/roteamento/abc/123', (req, res) => {res.sendFile(path.join(__dirname + '/view/roteamentoSimples/123.html'))});
+app.get('/home/roteamento', (req, res) => res.sendFile(path.join(__dirname, '/view/roteamentoSimples/roteamento.html')));
+app.get('/home/roteamento/abc', (req, res) => res.sendFile(path.join(__dirname, '/view/roteamentoSimples/abc.html')));
+app.get('/home/roteamento/abc/123', (req, res) => res.sendFile(path.join(__dirname, '/view/roteamentoSimples/123.html')));
+
+app.get('/home/form', (req, res) => res.sendFile(path.join(__dirname, '/view/roteamentoForm/index.html')))
+app.get('/home/form/saida', (req,res) => res.sendFile(path.join(__dirname, '/view/roteamentoForm/saida.html')));
 
 app.listen(3000, () => {
     console.log('Servidor ativo na porta 3000. Acesse http://localhost:3000/home');
